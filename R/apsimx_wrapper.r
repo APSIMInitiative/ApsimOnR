5732dd96704471f445f8b38d03791e0810697495
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2019-10-15 14:25:22 +0200

    functions help variables sel string /read_apsim_outpout

--- R/apsimx_wrapper.r
+++ R/apsimx_wrapper.r
@@ -182,11 +182,17 @@ read_apsimx_output <- function(dbFileName, tableName, variables) {
   con <- DBI::dbConnect(RSQLite::SQLite(), dbFileName)
 
   # Fetch all data from each table and store it in a list.
-  vars <- ''
-  for (var in variables) {
-    vars <- paste0(vars, 'Report.[', var, '], ')
-  }
-  vars <- paste0(vars, '_Simulations.Name as SimulationName')
+
+  # vars <- ''
+  # for (var in variables) {
+  #   vars <- paste0(vars, 'Report.[', var, '], ')
+  # }
+  #
+  # vars <- paste0(vars, '_Simulations.Name as SimulationName')
+
+  vars <- paste(sprintf("Report.[%s], ", variables),
+                "_Simulations.Name as SimulationName", collapse="")
+
   sql <- paste0('SELECT ', vars, ' FROM ', tableName, ', _Simulations WHERE _Simulations.ID = ', tableName, '.SimulationID')
   data <- DBI::dbGetQuery(con, sql)
   DBI::dbDisconnect(con)
